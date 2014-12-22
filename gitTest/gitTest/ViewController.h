f6d8983f6b3a1f34b47ce19268cd1316d011714a
zhouyong <494485961@qq.com>
2014-12-22 15:37:42 +0800

    第一次提交

---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by wangcy on 14-12-22.
+//  Copyright (c) 2014年 zjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by wangcy on 14-12-22.
+//  Copyright (c) 2014年 zjt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
